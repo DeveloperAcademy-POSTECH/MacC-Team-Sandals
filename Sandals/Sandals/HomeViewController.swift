6fdf37fb0be4c4094543064380ea79a774f1ec80
rbwo552 <rbwo552@naver.com>
2022-10-16 17:56:41 +0900

    개발을 하기 위해 개발환경을 구성했습니다.

---
+++ Sandals/Sandals/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Sandals
+//
+//  Created by rbwo on 2022/10/16.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Sandals")
+    }
+
+
+}
+
