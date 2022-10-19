9d69a74516fc756fcdce24b4955df61d7bb4f909
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2022-10-19 16:43:47 +0900

    [Feat] 킨디터 픽 섹션의 큐레이션 셀 정의

--- Kindy/Kindy/Views/CurationCollectionViewCell.swift
+++ Kindy/Kindy/Views/CurationCollectionViewCell.swift
@@ -11,4 +11,64 @@ import UIKit
 class CurationCollectionViewCell: UICollectionViewCell {
     
     static let reuseIdentifier = "CurationCollectionViewCell"
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 8
+        imageView.backgroundColor = .systemIndigo
+        
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .preferredFont(forTextStyle: .title3)
+        
+        return label
+    }()
+    
+    let numberLabel: UILabel = {
+        let label = UILabel()
+        label.layer.cornerRadius = 8
+        label.backgroundColor = .systemGray6
+        label.textColor = .white
+        label.font = .preferredFont(forTextStyle: .footnote)
+        
+        return label
+    }()
+    
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(imageView)
+        addSubview(titleLabel)
+        addSubview(numberLabel)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        numberLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            titleLabel.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 12),
+            titleLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 16)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Configure Cell
+    // TODO: TempCuration 나중에 바꿔야함
+    func configureCell(_ curation: TempCuration, index: IndexPath) {
+        titleLabel.text = curation.name
+        numberLabel.text = "\(index.item + 1) / \(index.count)"
+    }
 }
