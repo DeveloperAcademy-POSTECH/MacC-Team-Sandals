1f004aa5052f6b51f804a4d533f90815ade23387
rkddnr330 <rkddnr330@gmail.com>
2022-10-20 00:16:56 +0900

    [Feat] tableView 오토레이아웃 설정

--- Kindy/Kindy/Controllers/NearbyViewController.swift
+++ Kindy/Kindy/Controllers/NearbyViewController.swift
@@ -30,6 +30,14 @@ class NearbyViewController: UIViewController {
     
     func setupTableView() {
         view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
 
 }
