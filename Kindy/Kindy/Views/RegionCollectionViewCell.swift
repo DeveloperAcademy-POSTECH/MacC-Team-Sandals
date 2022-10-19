56e31bfbd4c67ac8c93d1273b24ffc514357bede
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2022-10-19 20:50:07 +0900

    [feat] 지역별 서점 지역 셀 정의

--- Kindy/Kindy/Views/RegionCollectionViewCell.swift
+++ Kindy/Kindy/Views/RegionCollectionViewCell.swift
@@ -8,7 +8,85 @@
 import UIKit
 
 // 지역별 서점 섹션의 지역 셀
+// TODO: Line view 클래스도 도 따로 만들어 보면 괜찮을듯?
 class RegionCollectionViewCell: UICollectionViewCell {
     
     static let reuseIdentifier = "RegionCollectionViewCell"
+    
+    let regionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        
+        return label
+    }()
+    
+    let topLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        
+        return view
+    }()
+    
+    let bottomLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        
+        return view
+    }()
+    
+    let rightLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        
+        return view
+    }()
+    
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(regionLabel)
+        addSubview(topLineView)
+        addSubview(bottomLineView)
+        addSubview(rightLineView)
+        
+        regionLabel.translatesAutoresizingMaskIntoConstraints = false
+        topLineView.translatesAutoresizingMaskIntoConstraints = false
+        bottomLineView.translatesAutoresizingMaskIntoConstraints = false
+        rightLineView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            regionLabel.topAnchor.constraint(equalTo: topAnchor),
+            regionLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            regionLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            regionLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            
+            topLineView.heightAnchor.constraint(equalToConstant: 1 / UIScreen.main.scale),
+            topLineView.topAnchor.constraint(equalTo: topAnchor),
+            topLineView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            topLineView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            bottomLineView.heightAnchor.constraint(equalToConstant: 1 / UIScreen.main.scale),
+            bottomLineView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            bottomLineView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            bottomLineView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            rightLineView.heightAnchor.constraint(equalToConstant: 1 / UIScreen.main.scale),
+            rightLineView.topAnchor.constraint(equalTo: topAnchor),
+            rightLineView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            rightLineView.leadingAnchor.constraint(equalTo: trailingAnchor),
+            rightLineView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Configure Cell
+    func configureCell(_ region: Region, hideTopLine: Bool, hideRightLine: Bool) {
+        regionLabel.text = region.name
+        topLineView.isHidden = hideTopLine
+        rightLineView.isHidden = hideRightLine
+    }
 }
