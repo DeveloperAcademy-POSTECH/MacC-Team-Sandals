7ba60b920f0dcbfe6101c2dff062242fe749ebda
rkddnr330 <rkddnr330@gmail.com>
2022-10-20 20:43:37 +0900

    [Feat] emptyView 레이아웃 설정, 내부 버튼 밑줄 추가
    - emptyView와 그 내부 요소의 레이아웃을 설정하였습니다.
    - extension을 이용하여 UIButton에 타이틀 텍스트의 밑줄을 긋는 메소드를 작성 및 구현하였습니다.

--- Kindy/Kindy/Controllers/Nearby/NearbyViewController.swift
+++ Kindy/Kindy/Controllers/Nearby/NearbyViewController.swift
@@ -112,13 +112,22 @@ extension UITableView {
         let messageLabel: UILabel = {
             let label = UILabel()
             label.text = message
+            label.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+            label.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 15)
+            label.textAlignment = .center
+            label.sizeToFit()
+            label.translatesAutoresizingMaskIntoConstraints = false
             
             return label
         }()
         
         let reportButton: UIButton = {
             let btn = UIButton()
-            btn.addTarget(self, action: #selector(reportButtonTapped), for: .touchUpInside)
+            btn.setTitle("독립서점 제보하기", for: .normal)
+            btn.setTitleColor(UIColor(red: 0.173, green: 0.459, blue: 0.355, alpha: 1), for: .normal)
+            btn.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 17)
+            btn.translatesAutoresizingMaskIntoConstraints = false
+            btn.setUnderline()
             
             return btn
         }()
@@ -126,6 +135,12 @@ extension UITableView {
         let emptyView : UIView = {
             let view = UIView()
             [messageLabel, reportButton].forEach{ view.addSubview($0) }
+            NSLayoutConstraint.activate([
+                messageLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                messageLabel.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 200),  // TODO: 서치바 기준으로 잡기
+                reportButton.centerXAnchor.constraint(equalTo: messageLabel.centerXAnchor),
+                reportButton.topAnchor.constraint(equalTo: messageLabel.topAnchor, constant: 26)
+            ])
             
             return view
         }()
@@ -140,5 +155,17 @@ extension UITableView {
     // TODO: '독립서점 제보하기' 버튼 액션 구현 (메일 앱으로 넘어가기)
     @objc func reportButtonTapped() {
         print("메일 앱으로 넘어가는 기능 구현하기")
+    }
+}
+
+// button에 underline 주기 위한 extension
+extension UIButton {
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count))
+        setAttributedTitle(attributedString, for: .normal)
     }
 }
