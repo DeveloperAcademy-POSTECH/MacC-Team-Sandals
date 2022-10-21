37d351c423542ee8a7b759408634aa8b91cdcb55
rkddnr330 <rkddnr330@gmail.com>
2022-10-21 19:37:46 +0900

    [Feat] 테이블 뷰 생성, 오토 레이아웃 설정

--- Kindy/Kindy/Controllers/RegionViewController.swift
+++ Kindy/Kindy/Controllers/RegionViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class RegionViewController: UIViewController {
 
+    private var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        setupTableView()
     }
 
+    private func setupTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
 }
