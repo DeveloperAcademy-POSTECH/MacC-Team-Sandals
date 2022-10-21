de5e6d3559cc15569cc7ff3ed3981dced670c43d
rkddnr330 <rkddnr330@gmail.com>
2022-10-21 19:40:31 +0900

    [Feat] RegionCell 기본 세팅

--- Kindy/Kindy/Views/Region/RegionCell.swift
+++ Kindy/Kindy/Views/Region/RegionCell.swift
@@ -8,5 +8,18 @@
 import UIKit
 
 class RegionCell: UITableViewCell {
+    
+    static let identifier = "RegionCell"
+    static let rowHeight: CGFloat = 104     // Cell 길이
+    
+    // MARK: - 라이프 사이클
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
 
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
