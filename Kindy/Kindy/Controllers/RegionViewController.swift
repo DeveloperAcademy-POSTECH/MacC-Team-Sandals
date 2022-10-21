1531af48966c70c5bf3e4be20820ed33e6e91567
rkddnr330 <rkddnr330@gmail.com>
2022-10-21 19:25:18 +0900

    [Feat] 지역 별 서점 목록 페이지 뷰 컨트롤러, 테이블 뷰 셀 생성

---
+++ Kindy/Kindy/Controllers/RegionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegionViewController.swift
+//  Kindy
+//
+//  Created by Park Kangwook on 2022/10/21.
+//
+
+import UIKit
+
+class RegionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ Kindy/Kindy/Views/Region/RegionCell.swift
@@ -0,0 +1,12 @@
+//
+//  RegionCell.swift
+//  Kindy
+//
+//  Created by Park Kangwook on 2022/10/21.
+//
+
+import UIKit
+
+class RegionCell: UITableViewCell {
+
+}
