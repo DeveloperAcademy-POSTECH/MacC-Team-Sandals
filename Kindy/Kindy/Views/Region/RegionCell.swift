f2117c449f7d676f25a5fe13f70f608b173e1a0e
rkddnr330 <rkddnr330@gmail.com>
2022-10-22 17:27:04 +0900

    [Feat] RegionCell에 들어갈 모델 프로퍼티 매치, 셀 재사용 메소드 추가

--- Kindy/Kindy/Views/Region/RegionCell.swift
+++ Kindy/Kindy/Views/Region/RegionCell.swift
@@ -12,44 +12,50 @@ class RegionCell: UITableViewCell {
     static let identifier = "RegionCell"
     static let rowHeight: CGFloat = 104     // Cell 길이
     
+    var bookstore: Dummy? {
+        didSet {
+            configureCell(item: bookstore!)
+        }
+    }
+    
     // MARK: - 프로퍼티
     
     private let photoImageView: UIImageView = {
         let view = UIImageView()
-        view.translatesAutoresizingMaskIntoConstraints = false
         view.contentMode = .scaleAspectFill
         view.layer.cornerRadius = 8
         view.clipsToBounds = true      // radius를 imageView에 적용
         view.backgroundColor = .lightGray      // 사진 없을 경우 default 색
+        view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
     }()
     
     private let infoStackView: UIStackView = {
         let view = UIStackView()
-        view.translatesAutoresizingMaskIntoConstraints = false
         view.alignment = .leading
         view.axis = .vertical
         view.spacing = 7   // 피그마에 적혀있는 4 적용하니 좁아보임. 7로 적용하니 피그마에 있는 비율과 비슷
+        view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
     }()
     
     private let nameLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 17)
         label.text = ""
+        label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 17)
+        label.translatesAutoresizingMaskIntoConstraints = false
 
         return label
     }()
     
     private let addressLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = ""
         label.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 15)
         label.textColor = UIColor(red: 0.459, green: 0.459, blue: 0.459, alpha: 1)
-        label.text = ""
+        label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
     }()
@@ -74,6 +80,12 @@ class RegionCell: UITableViewCell {
         ])
     }
     
+    private func configureCell(item: Dummy) {
+        nameLabel.text = item.name
+        addressLabel.text = item.address
+//        photoImageView.image = item.image?[0] ?? nil   // 첫번째 사진이 대표 사진
+    }
+    
     // MARK: - 라이프 사이클
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -85,5 +97,11 @@ class RegionCell: UITableViewCell {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        photoImageView.image = nil
+        nameLabel.text = nil
     }
 }
