60e7cabab6cdd985d3f6e772a01271de48ff199e
rkddnr330 <rkddnr330@gmail.com>
2022-10-22 17:35:15 +0900

    [Feat] tableView의 DataSource 구현 (filteredItem 생성, 셀 개수, 셀 내용, 헤더 텍스트)

--- Kindy/Kindy/Controllers/RegionViewController.swift
+++ Kindy/Kindy/Controllers/RegionViewController.swift
@@ -11,6 +11,8 @@ class RegionViewController: UIViewController {
 
     private var tableView = UITableView()
     
+    private var filteredItems: [Dummy] = dummyList
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +21,7 @@ class RegionViewController: UIViewController {
 
     private func setupTableView() {
         view.addSubview(tableView)
+        tableView.dataSource = self
         tableView.rowHeight = RegionCell.rowHeight
         tableView.register(RegionCell.self, forCellReuseIdentifier: RegionCell.identifier)   // Cell 등록 (코드 베이스라서)
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -29,5 +32,26 @@ class RegionViewController: UIViewController {
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
+    }
+}
+
+// MARK: - DataSource
+
+extension RegionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        filteredItems.count == 0 ? tableView.setEmptyView(text: "찾으시는 서점이 없으신가요?") : tableView.restore()
+        
+        return filteredItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RegionCell.identifier, for: indexPath) as? RegionCell else { return UITableViewCell() }
+        cell.bookstore = filteredItems[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return filteredItems.count == 0 ? nil : "총 \(filteredItems.count)개"
     }
 }
