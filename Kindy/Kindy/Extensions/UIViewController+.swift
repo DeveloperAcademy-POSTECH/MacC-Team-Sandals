2b2e540005ddb3f57731afe02484be3e97c717ed
rbwo552 <rbwo552@naver.com>
2022-10-23 21:11:44 +0900

    [Fix] : 빌드 에러, 변수명 수정

---
+++ Kindy/Kindy/Extensions/UIViewController+.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewController+.swift
+//  Kindy
+//
+//  Created by rbwo on 2022/10/18.
+//
+import UIKit
+
+extension UIViewController {
+    func gradientView(bounds: CGRect, colors: [CGColor]) -> CAGradientLayer {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors
+        
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.9)
+        return gradientLayer
+    }
+    
+    func dismissKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func hideKeyboard() {
+        self.view.window?.endEditing(true)
+    }
+}
