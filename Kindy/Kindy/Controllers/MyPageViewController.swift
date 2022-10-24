d8197f0e216d5f1e0350234bea1b2cc13b7b0a4c
100seo <97668338+100seo@users.noreply.github.com>
2022-10-24 21:44:36 +0900

    [Feat] 마이페이지 테이블 뷰의 데이터소스 구현

--- Kindy/Kindy/Controllers/MyPageViewController.swift
+++ Kindy/Kindy/Controllers/MyPageViewController.swift
@@ -7,23 +7,55 @@
 
 import UIKit
 
-class MyPageViewController: UIViewController {
+class MyPageViewController: UIViewController, UITableViewDelegate {
+    
+    let cellTitle: [String] = ["북마크한 서점", "라이선스", "이용약관", "독립서점 제보하기"]
+    
+    private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+        setupTableView()
 
+        self.navigationController?.navigationBar.topItem?.title = "마이페이지"
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 56
+        tableView.register(MyPageTableViewCell.self, forCellReuseIdentifier: "MyPageTableViewCell")
+                
     }
-    */
+    
+    func setupUI() {
+        self.view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        //테이블 뷰 라인 왼쪽 여백 없애기
+        tableView.separatorInset.left = 0
+    }
 
+}
+
+extension MyPageViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyPageTableViewCell", for: indexPath) as! MyPageTableViewCell
+        cell.myPageCellLabel.text = cellTitle[indexPath.row]
+        
+        return cell
+    }
+    
 }
