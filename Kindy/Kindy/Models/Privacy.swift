9f70b9f2fb6bf045e25082579f98bcdc351e5dce
100seo <97668338+100seo@users.noreply.github.com>
2022-10-25 00:06:14 +0900

    [Feat] Privacy 모델 추가

---
+++ Kindy/Kindy/Models/Privacy.swift
@@ -0,0 +1,14 @@
+//
+//  Privacy.swift
+//  Kindy
+//
+//  Created by baek seohyeon on 2022/10/24.
+//
+
+import Foundation
+
+// TODO: 이용약관과 라이선스, 본문을 추가해야합니다.
+struct Privacy {
+    let termsOfService: String = "이용약관입니다."
+    let license: String = "라이선스입니다."
+}
