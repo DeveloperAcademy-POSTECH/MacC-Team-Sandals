43ce8bcb83ebbe6361f2281ebe25556ebe0307d3
rbwo552 <rbwo552@naver.com>
2022-10-25 18:13:53 +0900

    [Feat] : 그라데이션 함수 수정

--- Kindy/Kindy/Extensions/UIView+.swift
+++ Kindy/Kindy/Extensions/UIView+.swift
@@ -2,7 +2,7 @@
 //  UIView+.swift
 //  Kindy
 //
-//  Created by rbwo on 2022/10/18.
+//  Created by rbwo on 2022/10/23.
 //
 
 import UIKit
@@ -10,23 +10,11 @@ import UIKit
 extension UIView {
     func gradientView(bounds: CGRect, colors: [CGColor]) -> CAGradientLayer {
         let gradientLayer = CAGradientLayer()
-        gradientLayer.locations = [0, 1]
         gradientLayer.frame = bounds
         gradientLayer.colors = colors
         
-        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.25)
-        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
-        
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.9)
         return gradientLayer
-    }
-    
-    func dismissKeyboard() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-    
-    @objc func hideKeyboard() {
-        self.view.window?.endEditing(true)
     }
 }
