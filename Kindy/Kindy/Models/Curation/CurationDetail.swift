449bfb254800ff36379487aba05051e7f5ebe34b
rbwo552 <rbwo552@naver.com>
2022-10-25 18:15:11 +0900

    [Feat] : 데이터 모델 수정

--- Kindy/Kindy/Models/Curation/CurationDetail.swift
+++ Kindy/Kindy/Models/Curation/CurationDetail.swift
@@ -9,9 +9,11 @@ import UIKit
 
 struct CurationDetail {
     let id: String
-    let text: String?
-    let images: [String : String]
-    let businessHour: String?
-    let address: String
-    let instaId: String?
+    let headText: String
+    let imageWithText: [(String, String)]
+    let infoText: String
+}
+
+extension CurationDetail {
+    static var item = CurationDetail(id: "", headText: "📚 두두디북스 | 부산 수영구 \n\n✏️ 출입문부터 시작해 내부 인테리어까지 프라이빗한 아지트에 온 것 같은 느낌이 들었다.  공간이 생각보다 굉장히 넓었고, 펍 운영 공간은 이전에 생각하던 서점 분위기와는 반전되는 느낌이라 신선하고 재밌었다.  2016년부터 계속 자리를 지키고 있는 만큼 ‘만남’에 진심인 이 곳에서는 굉장히 다양한 클래스들을 개최하고 있어 부산에 계신 분이라면 시간을 내어 참여해봐도 좋을 것 같다", imageWithText: [("testImage", "얘는 달팽이 책방으로서 달팽이를 키우며 어저고 저쩌고 난리가 20000 저만 ~"), ("testImage", "두번째 얘는 달팽이 책방으로서 달팽이를 키우며 어저고 저쩌고 난리가 20000 저만 ~"), ("testImage", "세번째 는 달팽이 책방으로서 달팽이를 키우며 어저고 저쩌고 난리가 20000 저만 ~")], infoText: "몇시부터 몇시 ~ \n 부산광역시 수영구 수영로510번길 43 (광안동, 광남빌라) 지하 1층 \n💬 @doodoodibooks")
 }
