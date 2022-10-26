a9420d9ace6f1ec4ddfd58ad08ba26b438e84d43
rkddnr330 <rkddnr330@gmail.com>
2022-10-26 21:04:20 +0900

    [Feat] 메일 기능 구현

--- Kindy/Kindy/Extensions/UITableView+.swift
+++ Kindy/Kindy/Extensions/UITableView+.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import MessageUI
 
-extension UITableView {
+extension UITableView: MFMailComposeViewControllerDelegate{
     func setEmptyView(text message: String) {
         let emptyView = EmptyView()
         emptyView.emptyViewMessage = message
@@ -21,6 +22,39 @@ extension UITableView {
     }
     
     @objc func reportButtonTapped() {
+        let viewController = self.findViewController()
+        let recipientEmail = "teamsandalsofficial@gmail.com"
+        let subject = "[독립서점 제보] 제목을 입력해주세요"
+        let body = "제보하려는 독립서점의 이름, 사진, 주소를 남겨주시면, 킨디에서 확인하고 업로드 할게요 :)"
         
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = viewController
+            mail.setToRecipients([recipientEmail])
+            mail.setSubject(subject)
+            mail.setMessageBody(body, isHTML: false)
+            
+            viewController?.present(mail, animated: true)
+            
+        } else if let emailUrl = createEmailUrl(to: recipientEmail, subject: subject, body: body) {
+            UIApplication.shared.open(emailUrl)
+        }
+    }
+    
+    private func createEmailUrl(to: String, subject: String, body: String) -> URL? {
+        let subjectEncoded = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        let bodyEncoded = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        
+        let gmailUrl = URL(string: "googlegmail://co?to=\(to)&subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        let outlookUrl = URL(string: "ms-outlook://compose?to=\(to)&subject=\(subjectEncoded)")
+        let defaultUrl = URL(string: "mailto:\(to)?subject=\(subjectEncoded)&body=\(bodyEncoded)")
+        
+        if let gmailUrl = gmailUrl, UIApplication.shared.canOpenURL(gmailUrl) {
+            return gmailUrl
+        } else if let outlookUrl = outlookUrl, UIApplication.shared.canOpenURL(outlookUrl) {
+            return outlookUrl
+        }
+        
+        return defaultUrl
     }
 }
--- Kindy/Kindy/Extensions/UIViewController+.swift
+++ Kindy/Kindy/Extensions/UIViewController+.swift
@@ -5,6 +5,7 @@
 //  Created by rbwo on 2022/10/18.
 //
 import UIKit
+import MessageUI
 
 extension UIViewController {
     func gradientView(bounds: CGRect, colors: [CGColor]) -> CAGradientLayer {
@@ -25,5 +26,35 @@ extension UIViewController {
     
     @objc func hideKeyboard() {
         self.view.window?.endEditing(true)
+    }
+}
+
+extension UIViewController: MFMailComposeViewControllerDelegate {
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+        showMailAlert(result: result)
+    }
+    
+    private func showMailAlert(result: MFMailComposeResult) {
+        let sheet = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        switch result {
+        case .sent:
+            sheet.title = "제보 성공"
+            sheet.message = "메일이 성공적으로 전송되었습니다."
+        case .failed:
+            sheet.title = "메일 전송 실패"
+            sheet.message = "인터넷 환경 또는 기기 메일 설정을 확인해주세요."
+        case .saved:
+            sheet.title = "메일 저장"
+            sheet.message = "제보 메일이 저장되었습니다."
+        case .cancelled:
+            sheet.title = "제보 취소"
+            sheet.message = "다음에 꼭 제보해주세요 :)"
+        @unknown default:
+            fatalError()
+        }
+        
+        sheet.addAction(UIAlertAction(title: "확인", style: .cancel))
+        present(sheet, animated: true)
     }
 }
