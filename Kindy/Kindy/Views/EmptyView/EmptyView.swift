726a14cf32eda017b68298c4b65df177b57d9939
rkddnr330 <rkddnr330@gmail.com>
2022-10-26 20:58:01 +0900

    [Feat] EmptyView 따로 분리

---
+++ Kindy/Kindy/Views/EmptyView/EmptyView.swift
@@ -0,0 +1,85 @@
+//
+//  EmptyView.swift
+//  Kindy
+//
+//  Created by Park Kangwook on 2022/10/26.
+//
+
+import UIKit
+
+final class EmptyView: UIView {
+    
+    var emptyViewMessage: String? {
+        didSet {
+            configure(message: emptyViewMessage!)
+        }
+    }
+    
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        label.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 15)
+        label.textAlignment = .center
+        label.sizeToFit()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    let reportButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("독립서점 제보하기", for: .normal)
+        btn.setTitleColor(UIColor(red: 0.173, green: 0.459, blue: 0.355, alpha: 1), for: .normal)
+        btn.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 17)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setUnderline()
+        
+        return btn
+    }()
+    
+    private let emptyView : UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    // MARK: - 라이프 사이클
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - 메소드
+    
+    private func setupView() {
+        [messageLabel, reportButton].forEach{ emptyView.addSubview($0) }
+        self.addSubview(emptyView)
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor),
+            messageLabel.centerYAnchor.constraint(equalTo: emptyView.topAnchor, constant: 200),  // TODO: 서치바 기준으로 잡기
+            
+            reportButton.centerXAnchor.constraint(equalTo: messageLabel.centerXAnchor),
+            reportButton.topAnchor.constraint(equalTo: messageLabel.topAnchor, constant: 26),
+            
+            emptyView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            emptyView.topAnchor.constraint(equalTo: topAnchor),
+            emptyView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            emptyView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+
+    }
+    
+    private func configure(message: String) {
+        messageLabel.text = message
+    }
+    
+}
