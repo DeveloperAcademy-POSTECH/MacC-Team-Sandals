406d562c871d7de874fc43544a2a72ea1bfda1d6
profit0124 <profit0124@gmail.com>
2022-10-28 00:12:07 +0900

    enum 초기화 함수를 추가함으로서, HomeView 나올때 데이터 업데이트 적용

--- Kindy/Kindy/Models/Item.swift
+++ Kindy/Kindy/Models/Item.swift
@@ -9,11 +9,21 @@ import Foundation
 
 // 메인 Diffable Data Source에 쓰일 Item 열거형
 enum Item: Hashable {
+    case mainCuration(Curation)
     case curation(Curation)
     case bookStore(Bookstore)
+    case bookmarkedBookStore(Bookstore)
     case region(Region)
     case emptyNearby
     case emptyBookmark
+    
+    var mainCuration: Curation? {
+        if case .mainCuration(let mainCuration) = self {
+            return mainCuration
+        } else {
+            return nil
+        }
+    }
     
     var curation: Curation? {
         if case .curation(let curation) = self {
@@ -31,6 +41,14 @@ enum Item: Hashable {
         }
     }
     
+    var bookmarkedBookStore: Bookstore? {
+        if case .bookmarkedBookStore(let bookmarkedBookStore) = self {
+            return bookmarkedBookStore
+        } else {
+            return nil
+        }
+    }
+    
     var region: Region? {
         if case .region(let region) = self {
             return region
@@ -40,16 +58,16 @@ enum Item: Hashable {
     }
     
     // 같은 값이 들어가면 안됨(unique 해야함) -> 어떻게 구별? 다른 큐레이션 구조체(큐레이션 프로토콜 만들어도 괜찮으려나)를 만들어야하나 아니면 아이템의 케이스 하나 더 추가
-    static let mainCuration: [Item] = [
-        .curation(NewItems.mainDummy[0])
+    static var mainCuration: [Item] = [
+        .mainCuration(NewItems.curationDummy[Int.random(in: 0..<NewItems.curationDummy.count)])
     ]
     
-    static let curations: [Item] = NewItems.curationDummy.filter{ $0.id != "1" }.map{ .curation($0) }
+    static var curations: [Item] = NewItems.curationDummy.filter{ $0.id != "1" }.map{ .curation($0) }
     
     // MARK 추후 데이터 로직으로 filter와 sort를 이용해 거리순으로 제거 후 입력 받는다
-    static let nearByBookStores: [Item] = NewItems.bookstoreDummy.sorted{ $0.name > $1.name }.map{ .bookStore($0) }
+    static var nearByBookStores: [Item] = NewItems.bookstoreDummy.sorted{ $0.name > $1.name }.map{ .bookStore($0) }
     
-    static let bookmarkedBookStores: [Item] = NewItems.bookstoreDummy.filter{ $0.isFavorite }.map{ .bookStore($0) }
+    static var bookmarkedBookStores: [Item] = NewItems.bookstoreDummy.filter{ $0.isFavorite }.map{ .bookmarkedBookStore($0) }
     
     static let regions: [Item] = [
         .region(Region(name: "전체")),
@@ -62,4 +80,11 @@ enum Item: Hashable {
         .region(Region(name: "경남/울산/부산")),
         .region(Region(name: "제주"))
     ]
+    
+    static func updateBookmarkedData() {
+        bookmarkedBookStores = NewItems.bookstoreDummy.filter{ $0.isFavorite }.map{ .bookmarkedBookStore($0) }
+    }
+    
 }
+   
+    
