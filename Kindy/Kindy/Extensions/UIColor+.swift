c3bc2c430d759bf092685596a5faf9dfefe984d2
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2022-10-30 13:05:16 +0900

    [Refactor] UIFont, UIColor extension 추가

--- Kindy/Kindy/Extensions/UIColor+.swift
+++ Kindy/Kindy/Extensions/UIColor+.swift
@@ -8,5 +8,9 @@
 import UIKit
 
 extension UIColor {
-    
+    static let kindyGray = UIColor(named: "kindyGray")
+    static let kindyLightGray = UIColor(named: "kindyLightGray")
+    static let kindyPrimaryGreen = UIColor(named: "kindyPrimaryGreen")
+    static let kindySecondaryGreen = UIColor(named: "kindySecondaryGreen")
+    static let kindySecondaryLightGreen = UIColor(named: "kindySecondaryLightGreen ")
 }
