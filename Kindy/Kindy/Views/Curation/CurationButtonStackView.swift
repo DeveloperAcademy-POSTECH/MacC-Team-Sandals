41fb68afefb8ef703abe074f96802cc287695bac
rbwo552 <rbwo552@naver.com>
2022-11-18 19:17:07 +0900

    [Feat, Refactor] : 댓글 셀 추가, 좋아요 셀 추가, 좋아요 버튼 액션 로직 구현, 동적 셀 리팩토링

--- Kindy/Kindy/Views/Curation/CurationButtonStackView.swift
+++ Kindy/Kindy/Views/Curation/CurationButtonStackView.swift
@@ -2,19 +2,53 @@
 //  CurationButtonStackView.swift
 //  Kindy
 //
-//  Created by rbwo on 2022/11/18.
+//  Created by rbwo on 2022/11/09.
 //
 
 import UIKit
 
-class CurationButtonStackView: UIView {
+// MARK: reply 기능은 추가가 되면 주석 해제
+final class CurationButtonStackView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private let curation: Curation
+
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .horizontal
+        view.distribution = .fill
+        view.spacing = 24
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private lazy var heartView: UIView = {
+        let view = CurationButtonItemView(frame: .zero, curation: curation, viewName: .heart)
+        return view
+    }()
+
+    private lazy var replyView: UIView = {
+        let view = CurationButtonItemView(frame: .zero, curation: curation, viewName: .reply)
+        return view
+    }()
+
+    init(frame: CGRect, curation: Curation) {
+        self.curation = curation
+        super.init(frame: frame)
+        setupUI()
     }
-    */
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        self.addSubview(stackView)
+        stackView.addArrangedSubview(heartView)
+//        stackView.addArrangedSubview(replyView)
+
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16)
+        ])
+    }
 }
--- Kindy/Kindy/Views/Curation/CurationHeaderView.swift
+++ Kindy/Kindy/Views/Curation/CurationHeaderView.swift
@@ -1,5 +1,5 @@
 //
-//  CurationHeaderView2.swift
+//  CurationHeaderView.swift
 //  Kindy
 //
 //  Created by rbwo on 2022/10/23.
@@ -7,16 +7,17 @@
 
 import UIKit
 
-class CurationHeaderView: UIView {
-    
+final class CurationHeaderView: UIView {
+
     lazy var imageView: UIImageView = {
         let view = UIImageView()
         view.contentMode = .scaleAspectFill
         view.clipsToBounds = true
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .kindyLightGray
         return view
     }()
-    
+
     private lazy var titleLabel: UILabel = {
         let view = UILabel()
         view.textColor = .white
@@ -25,7 +26,7 @@ class CurationHeaderView: UIView {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
+
     private lazy var subtitleLabel: UILabel = {
         let view = UILabel()
         view.textColor = .white
@@ -34,38 +35,38 @@ class CurationHeaderView: UIView {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-   
+
     init(frame: CGRect, curation: Curation) {
         super.init(frame: frame)
         self.configureUI()
         self.configureData(curationData: curation)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func configureUI(){
         self.addSubview(imageView)
         self.addSubview(titleLabel)
         self.addSubview(subtitleLabel)
-        
+
         let defaultHeight: CGFloat = (0.65 * screenHeight + 96.5) - (0.52 * screenHeight)
-        
+
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: topAnchor),
             imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
             imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
             imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            
+
             titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -defaultHeight),
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            
+
             subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 14),
             subtitleLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor)
         ])
     }
-    
+
     private func configureData(curationData: Curation) {
         self.titleLabel.text = curationData.title
         self.subtitleLabel.text = curationData.subTitle
