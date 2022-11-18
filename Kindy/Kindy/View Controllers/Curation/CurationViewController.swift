34bb80a610675dab7c2abc0f221e071a6432c651
rbwo552 <rbwo552@naver.com>
2022-11-18 19:14:13 +0900

    [Feat] : reply 셀, 더미데이터 추가, 서점 정보 navigation controller로 present

--- Kindy/Kindy/View Controllers/Curation/CurationViewController.swift
+++ Kindy/Kindy/View Controllers/Curation/CurationViewController.swift
@@ -8,14 +8,13 @@
 import UIKit
 
 final class CurationViewController: UIViewController {
-    
-    weak var cellDelegate: DynamicCell?
-    
+
+    let replyDummy = [["니버", "좋네요 너무 가보고 싶어요 ! 좋네요 가보고 싶어요 ! 좋네요 너무 가보고 싶어요 ! 좋네요 너무 가보고 싶어요 ! 좋네요 너무 가보고 싶어요 ! 좋네요 너무 가보고 싶어요 !", "2022-11-04 •"], ["니버", "어쩌라구용~", "2022-11-04 •"], ["니버", "좋네요 너무 가보고 싶어요  좋네요 너무 가보고 싶어요  좋네요 너무 가보고 싶어요 !", "2022-11-04 •"], ["니버", "별룬데요 !", "2022-11-04 • "], ["니버", " 굿! ", "2022-11-04 • "]]
+
     private var curation: Curation
-    
-    private var cellCount = 0
-    
-    private var cellHeight: CGFloat = 0
+
+    private var cellCount: Int = 0
+    private var replyCount: Int = 0
 
     private var bookstoresRequestTask: Task<Void, Never>?
     private let firestoreManager = FirestoreManager()
@@ -28,13 +27,16 @@ final class CurationViewController: UIViewController {
         self.images = images
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private lazy var collectionView: UICollectionView = {
-        let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout.init())
+        let layout = UICollectionViewFlowLayout()
+        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
         view.backgroundColor = .white
         view.dataSource = self
         view.delegate = self
@@ -49,7 +51,13 @@ final class CurationViewController: UIViewController {
             bookstoresRequestTask = nil
         }
     }
-    
+
+    private lazy var bottomView: UIView = {
+        let view = CurationButtonStackView(frame: .zero, curation: curation)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
@@ -60,20 +68,28 @@ final class CurationViewController: UIViewController {
 private extension CurationViewController {
     func configureCollectionView() {
         collectionView.register(CurationStoreCell.self, forCellWithReuseIdentifier: CurationStoreCell.identifier)
-        
+
         collectionView.register(CurationTextCell.self, forCellWithReuseIdentifier: CurationTextCell.identifier)
-        
+
         collectionView.register(CurationDetailCell.self, forCellWithReuseIdentifier: CurationDetailCell.identifier)
+
+        collectionView.register(CurationReplyCell.self, forCellWithReuseIdentifier: CurationReplyCell.identifier)
     }
-    
+
     func createLayout() {
         view.addSubview(collectionView)
-        
+        view.addSubview(bottomView)
+
         NSLayoutConstraint.activate([
             collectionView.topAnchor.constraint(equalTo: view.topAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -72),
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+
+            bottomView.topAnchor.constraint(equalTo: collectionView.bottomAnchor),
+            bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
 }
@@ -85,19 +101,17 @@ extension CurationViewController: UICollectionViewDataSource {
                 return UICollectionViewCell() }
             cell.configure(image: images[0], bookStore: curation.bookstoreID)
             cell.backgroundColor = .clear
-            cell.layer.zPosition = 1
             return cell
         }
-
-        else if indexPath.item == 1 || indexPath.item == cellCount{
+        else if indexPath.item == 1 {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CurationTextCell.identifier, for: indexPath) as? CurationTextCell else { return UICollectionViewCell() }
-            if indexPath.item == cellCount {
-                cell.alpha = 0
-            }
             cell.headConfigure(data: curation)
             return cell
+        } else if indexPath.item > cellCount {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CurationReplyCell.identifier, for: indexPath) as? CurationReplyCell else { return UICollectionViewCell() }
+            cell.configure(data: replyDummy[replyCount - indexPath.row])
+            return cell
         }
-
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CurationDetailCell.identifier, for: indexPath) as? CurationDetailCell else { return UICollectionViewCell() }
 
         cell.configure(description: curation.descriptions[indexPath.item - 2], image: images[indexPath.item - 1])
@@ -106,44 +120,30 @@ extension CurationViewController: UICollectionViewDataSource {
 }
 extension CurationViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        cellCount += 2
+        cellCount += 1
         cellCount += curation.descriptions.count
         return cellCount + 1
+        // TODO: replyDummy 를 큐레이션 모델 내부에 reply를 받아와서 count 하는 코드로 변경
+//        replyCount = cellCount + replyDummy.count
+//        return replyCount + 1
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if indexPath.item == 0 {
-            let vc = DetailBookstoreViewController()
-            vc.bookstore = bookStore
-            show(vc, sender: nil)
+            let bookstoreVC = DetailBookstoreViewController()
+            bookstoreVC.bookstore = bookStore
+            let naviVC = UINavigationController(rootViewController: bookstoreVC)
+            naviVC.modalPresentationStyle = .overFullScreen
+            show(naviVC, sender: nil)
         }
     }
 }
 
 extension CurationViewController: UICollectionViewDelegateFlowLayout {
-    /// 셀 크기
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
-        let width: CGFloat = UIScreen.main.bounds.width
-        let height: CGFloat = UIScreen.main.bounds.height
-    
-        if indexPath.item == 0 {
-            return CGSize(width: width, height: height / 7)
-        } else if indexPath.item == 1 || indexPath.item == cellCount {
-            let height: Double = Double(curation.headText.count) / 27.2 * 20
-            return CGSize(width: width, height: height + 100)
-        }
-        else {
-            let height: Double = Double(curation.descriptions[indexPath.row - 2].content!.count) / 27.2 * 20
-            // image height를 고정한다면 밑에 더해주면 댐다
-            return CGSize(width: width, height: height + 400)
-        }
-    }
-
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return CGFloat(0)
     }
