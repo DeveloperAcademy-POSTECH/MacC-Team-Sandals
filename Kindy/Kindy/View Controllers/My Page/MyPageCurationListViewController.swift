2325d9008aa6c353ba74ae5eacf941ef4875c233
YunJoon <kpl88@naver.com>
2022-12-03 23:03:07 +0900

    [Feat] 파일명 수정 및 내가 쓴 글, 좋아요 한 글, 댓글 단 글 가져오기 기능 추가

--- Kindy/Kindy/View Controllers/My Page/MyPageCurationListViewController.swift
+++ Kindy/Kindy/View Controllers/My Page/MyPageCurationListViewController.swift
@@ -10,28 +10,22 @@ import UIKit
 // 어느 셀을 누르고 들어왔는지 판단하기 위한 enum
 enum PreviousSelectedCell {
     case myCuration
-//    case mySmallTalk
     case likeList
     case commentList
 }
 
-final class WritingListViewController: UIViewController {
-
+final class MyPageCurationListViewController: UIViewController {
+    
     // MARK: Properties
     private var curationsRequestTask: Task<Void, Never>?
     private var imageRequestTask: Task<Void, Never>?
     private var userRequestTask: Task<Void, Never>?
     
-    deinit {
-        curationsRequestTask?.cancel()
-        imageRequestTask?.cancel()
-        userRequestTask?.cancel()
-    }
-    
     var previousSelectedCell: PreviousSelectedCell = .myCuration
-
+    
     private let writingTableView: UITableView = {
         let tableView = UITableView()
+        tableView.separatorInset.left = 0
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
@@ -39,19 +33,26 @@ final class WritingListViewController: UIViewController {
     // 글이 없을때 보여줄 뷰
     private let noWritingView = NoWritingView()
     
-    private var writings: [Curation] = []
+    private var curations: [Curation] = []
+    private var kinditorOfCuration: [String : String] = [:]
     
     // MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
         setupUI()
-        fetchCurations()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         setupNavigationBar()
         tabBarController?.tabBar.isHidden = true
+        fetchCurations()
+    }
+    
+    deinit {
+        curationsRequestTask?.cancel()
+        imageRequestTask?.cancel()
+        userRequestTask?.cancel()
     }
     
     // MARK: Helpers
@@ -64,10 +65,6 @@ final class WritingListViewController: UIViewController {
     
     private func setupUI() {
         view.backgroundColor = .white
-        guard !writings.isEmpty else {
-            setupNoWritingView()
-            return
-        }
         
         view.addSubview(writingTableView)
         NSLayoutConstraint.activate([
@@ -76,6 +73,74 @@ final class WritingListViewController: UIViewController {
             writingTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             writingTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+    }
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.topItem?.title = ""
+        navigationController?.navigationBar.tintColor = UIColor.black
+        
+        switch previousSelectedCell {
+        case .myCuration:
+            navigationItem.title = "내 글 목록"
+            //        case .smallTalk:
+            //            navigationItem.title = ""
+        case .likeList:
+            navigationItem.title = "좋아요 한 글"
+        case .commentList:
+            navigationItem.title = "댓글 단 글"
+        }
+    }
+    
+    private func fetchCurations() {
+        let userID = UserManager().getID()
+        curationsRequestTask?.cancel()
+        
+        curationsRequestTask = Task {
+            switch previousSelectedCell {
+            case .myCuration:
+                if let curations = try? await CurationRequest().fetchMyCuration(userID: userID) {
+                    processAfterCurationRequest(curations)
+                } else {
+                    setupNoWritingView()
+                }
+                
+            case .likeList:
+                if let curations = try? await CurationRequest().fetchLikeCurations(userID: userID) {
+                    processAfterCurationRequest(curations)
+                } else {
+                    setupNoWritingView()
+                }
+                
+            case .commentList:
+                if let curations = try? await CurationRequest().fetchCommentedCurations(userID: userID) {
+                    processAfterCurationRequest(curations)
+                } else {
+                    setupNoWritingView()
+                }
+            }
+            curationsRequestTask = nil
+        }
+    }
+    
+    private func processAfterCurationRequest(_ curations: [Curation]) {
+        self.curations = curations
+        
+        guard !self.curations.isEmpty else {
+            setupNoWritingView()
+            curationsRequestTask = nil
+            return
+        }
+        
+        userRequestTask?.cancel()
+        userRequestTask = Task {
+            for curation in self.curations {
+                if let nickname = try? await UserManager().fetch(with: curation.userID).nickName {
+                    self.kinditorOfCuration[curation.userID] = nickname
+                }
+            }
+            userRequestTask = nil
+            writingTableView.reloadData()
+        }
     }
     
     private func setupNoWritingView() {
@@ -87,8 +152,6 @@ final class WritingListViewController: UIViewController {
             noWritingView.noWritingLabel.text = "내 글이 메인에 소개될 기회를 잡아보세요!"
             noWritingView.noWritingButton.setTitle("큐레이션 작성하기", for: .normal)
             noWritingView.noWritingButton.addTarget(self, action: #selector(writeCurationButtonTapped), for: .touchUpInside)
-            
-//        case .smallTalk:
             
         case .likeList:
             noWritingView.noWritingLabel.text = "아직 좋아요 한 글이 없어요"
@@ -111,45 +174,6 @@ final class WritingListViewController: UIViewController {
         ])
     }
     
-    // TODO: 조건에 맞는 큐레이션 들고오기
-    private func fetchCurations() {
-//        curationsRequestTask?.cancel()
-//
-////        switch previousSelectedCell {
-////        case .myCuration:
-////            <#code#>
-////        case .likeList:
-////            <#code#>
-////        case .commentList:
-////            <#code#>
-////        }
-//        curationsRequestTask = Task {
-//            if let curations = try? await CurationRequest().fetch() {
-//
-//            } else {
-//                writings = []
-//            }
-//            curationsRequestTask = nil
-//        }
-        writingTableView.reloadData()
-    }
-    
-    private func setupNavigationBar() {
-        navigationController?.navigationBar.topItem?.title = ""
-        navigationController?.navigationBar.tintColor = UIColor.black
-        
-        switch previousSelectedCell {
-        case .myCuration:
-            navigationItem.title = "내 글 목록"
-//        case .smallTalk:
-//            navigationItem.title = ""
-        case .likeList:
-            navigationItem.title = "좋아요 한 글"
-        case .commentList:
-            navigationItem.title = "댓글 단 글"
-        }
-    }
-    
     // MARK: Actions
     @objc func writeCurationButtonTapped() {
         print("DEBUG: TO WRITE")
@@ -164,35 +188,42 @@ final class WritingListViewController: UIViewController {
 
 // MARK: Extensions
 // MARK: UITableViewDataSource
-extension WritingListViewController: UITableViewDataSource {
+extension MyPageCurationListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return writings.count
+        return curations.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CurationListCell.identifier, for: indexPath) as? CurationListCell else { return UITableViewCell() }
-        cell.curation = writings[indexPath.row]
+        
+        let currentCuration = curations[indexPath.row]
+        cell.curation = currentCuration
         
         imageRequestTask = Task {
-            let descriptions = writings[indexPath.row].descriptions
-            if let image = try? await ImageCache.shared.load(descriptions[indexPath.row].image) {
+            if let image = try? await ImageCache.shared.load(currentCuration.mainImage) {
                 cell.photoImageView.image = image
             } else {
                 cell.photoImageView.image = UIImage()
             }
             imageRequestTask = nil
         }
+        
+        cell.kinditor = kinditorOfCuration[cell.curation?.userID ?? "킨디"]
+        
+        let userID = UserManager().getID()
+        cell.curationIsLiked = (cell.curation?.likes ?? []).contains(userID)
+        
         return cell
     }
-
+    
 }
 
 // MARK: UITableViewDelegate
-extension WritingListViewController: UITableViewDelegate {
+extension MyPageCurationListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let curationVC = PagingCurationViewController(curation: writings[indexPath.row])
+        let curationVC = PagingCurationViewController(curation: curations[indexPath.row])
         curationVC.modalPresentationStyle = .overFullScreen
         curationVC.modalTransitionStyle = .crossDissolve
         present(curationVC, animated: true)
