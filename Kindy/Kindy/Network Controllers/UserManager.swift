ac7ca8fa892f7cb95e2be1bdb1b065d04fb22ee6
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2022-12-04 17:31:22 +0900

    [refactor] Firestore 프로토콜 리팩토링
    - 반복되는 코드를 줄였습니다.
    - UserManager는 나중에 리팩토링하겠습니다.

--- Kindy/Kindy/Network Controllers/UserManager.swift
+++ Kindy/Kindy/Network Controllers/UserManager.swift
@@ -6,9 +6,9 @@
 //
 
 import Foundation
-import FirebaseFirestoreSwift
 import FirebaseAuth
 import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 struct UserManager: FirestoreRequest {
     typealias Response = User
@@ -24,12 +24,12 @@ extension UserManager {
     
     /// 현재 로그인되어 있는 정보로 유저 데이터 fetch
     func fetchCurrentUser() async throws -> User {
-        return try await db.collection(collectionPath).document(Auth.auth().currentUser?.uid ?? "").getDocument(as: User.self)
+        return try await documentReference(Auth.auth().currentUser?.uid ?? "").getDocument(as: User.self)
     }
     
     /// 북마크 데이터 변경
     func updateBookmark(email: String, provider: String, bookmarkedBookstores: [String]) async throws {
-        let querySnapshot = try await db.collection(collectionPath).whereField("email", isEqualTo: email).whereField("provider", isEqualTo: provider).getDocuments()
+        let querySnapshot = try await collectionReference.whereField("email", isEqualTo: email).whereField("provider", isEqualTo: provider).getDocuments()
         let document = querySnapshot.documents.first
         try await document?.reference.updateData(["bookmarkedBookstores" : bookmarkedBookstores])
     }
@@ -50,7 +50,7 @@ extension UserManager {
     /// 이미 가입한 유저인지 확인
     func isExistingUser(_ email: String?, _ provider: String) async throws -> Bool {
         if let email = email {
-            return try await !db.collection(collectionPath).whereField("email", isEqualTo: email).whereField("provider", isEqualTo: provider).getDocuments().documents.map{ $0.documentID }.isEmpty
+            return try await !collectionReference.whereField("email", isEqualTo: email).whereField("provider", isEqualTo: provider).getDocuments().documents.map{ $0.documentID }.isEmpty
         } else {
             return false
         }
@@ -60,7 +60,7 @@ extension UserManager {
     func delete() {
         let auth = Auth.auth().currentUser
         
-        db.collection(collectionPath).document(auth?.uid ?? "al").delete() { _ in
+        documentReference(auth?.uid ?? "al").delete() { _ in
             auth?.delete()
         }
     }
@@ -70,38 +70,36 @@ extension UserManager {
 extension UserManager {
     /// 닉네임 중복확인
     func isExistingNickname(_ nickName: String) async throws -> Bool {
-        let nickNames = try await db.collection(collectionPath).getDocuments().documents.map{ $0.data() }.filter{ String(describing: $0["nickName"]!) == nickName }
+        let nickNames = try await collectionReference.getDocuments().documents.map{ $0.data() }.filter{ String(describing: $0["nickName"]!) == nickName }
         return !nickNames.isEmpty
     }
 
     /// 닉네임 수정
     func editNickname(_ newNickname: String) {
-        let user = db.collection(collectionPath).document(Auth.auth().currentUser?.uid ?? "")
-        user.updateData(["nickName": newNickname])
+        documentReference(Auth.auth().currentUser?.uid ?? "").updateData(["nickName": newNickname])
     }
 }
 
-/// 마이페이지에서 사용하는 CurationRequest
+// MARK: 마이페이지에서 사용하는 CurationRequest
 extension UserManager {
-    
-    // 내가 작성한 큐레이션
+    /// 내가 작성한 큐레이션 fetch
     func fetchMyCurations(userID: String) async throws -> [Curation] {
         let querySnapShot = try await db.collection(CollectionPath.curations).whereField("userID", isEqualTo: userID).getDocuments()
         let document = try querySnapShot.documents.map { try $0.data(as: Curation.self) }
         return document.reversed()
     }
     
-    // 좋아요 한 큐레이션
+    /// 좋아요 한 큐레이션 fetch
     func fetchLikedCurations(userID: String) async throws -> [Curation] {
         let querySnapshot = try await db.collection(CollectionPath.curations).whereField("likes", arrayContains: userID).getDocuments()
         let document = try querySnapshot.documents.map { try $0.data(as: Curation.self) }
         return document.reversed()
     }
     
-    // 댓글 단 큐레이션
+    /// 댓글 단 큐레이션 fetch
     func fetchCommentedCurations(userID: String) async throws -> [Curation] {
         // 현재 유저의 document 데이터를 가져와서
-        let documentSnapshot = try await db.collection(CollectionPath.users).document(userID).getDocument()
+        let documentSnapshot = try await documentReference(userID).getDocument()
         let user = try documentSnapshot.data(as: User.self)
         var commentedCurations: [Curation] = []
         
@@ -128,7 +126,7 @@ extension UserManager {
     
     // 유저의 댓글 단 큐레이션 필드에 큐레이션 id 추가
     func addCommentedCuration(userID: String, curationID: String) throws {
-        let field = db.collection(CollectionPath.users).document(userID)
+        let field = db.collection(collectionPath).document(userID)
         // arrayUnion: 현재 댓글을 작성한 큐레이션의 id가 유저의 필드에 이미 있다면 중복으로 업데이트하지 않음
         field.updateData(["commentedCurations": FieldValue.arrayUnion([curationID])])
     }
@@ -140,7 +138,7 @@ extension UserManager {
         
         switch myComments.count {
         case 1:
-            let field = db.collection(CollectionPath.users).document(userID)
+            let field = db.collection(collectionPath).document(userID)
             try await field.updateData(["commentedCurations": FieldValue.arrayRemove([curationID])])
             
         default:
@@ -162,7 +160,7 @@ extension UserManager {
             guard !repeatedUserID.contains(currentUserID) else { continue }
             
             repeatedUserID.append(currentUserID)
-            let field = db.collection(CollectionPath.users).document(currentUserID)
+            let field = db.collection(collectionPath).document(currentUserID)
             try await field.updateData(["commentedCurations": FieldValue.arrayRemove([curationID])])
         }
     }
