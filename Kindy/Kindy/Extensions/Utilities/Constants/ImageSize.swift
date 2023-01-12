7e259970bf9b3b6d29fa5c30b47d78551a564aaa
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-01-12 21:26:47 +0900

    [feat] 킹피셔 패키지 추가
    - SPM으로 킹피셔 추가

---
+++ Kindy/Kindy/Extensions/Utilities/Constants/ImageSize.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum ImageSize {
+    static let large = CGSize(width: 500, height: 500)
+    static let medium = CGSize(width: 300, height: 300)
+    static let small = CGSize(width: 100, height: 100)
+}
